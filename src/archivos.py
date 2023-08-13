1b4d7e7a5870b7ed8a4807f890c6c2a3fcf94fc0
Isabella Forte <isabellaannaforte@gmail.com>
2023-08-12 22:27:00 -0300

    func leer archivo + agrego csv

---
+++ src/archivos.py
@@ -0,0 +1,10 @@
+#Lectura del csv
+import csv
+
+def readFile():
+    barcos = []
+    with open(r"ships.csv") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            barcos.append(row)
+    return barcos
