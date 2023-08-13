f6c3d6a83e4e2851a90512bb414c4c907acd4a50
Thiago Monteros <thmonteros2@gmail.com>
2023-08-12 21:04:39 -0300

    prueba terminada y comienzo de tests

--- src/prueba.py
+++ src/prueba.py
@@ -1,16 +1,15 @@
 class Ship:
+  max_ = 200
+  def __init__(self, draft, crew):
+         self.draft = draft
+         self.crew = crew
 
-   def __init__(self, draft, crew):
-       self.draft = draft
-       self.crew = crew
-       max_ = 200
-
-   def get_draft(self):
-       return self.draft
-   def get_crew(self):
-       return self.crew
-   def is_worth_it(self)->float:
-      return (self.draft-self.crew*1.5)
+  def get_draft(self):
+         return self.draft
+  def get_crew(self):
+        return self.crew
+  def is_worth_it(self)->float:
+        return (self.draft-(self.crew*1.5))
 
 
 class Cargo(Ship):
@@ -18,11 +17,12 @@ class Cargo(Ship):
        Ship.__init__(self, draft, crew)
        self.cargo = cargo
        self.quality = quality
+
    def is_worth_it(self) ->float:
        self.draft - (self.crew*1.5)
 
        if self.draft <= 0:
-           raise Exception
+           raise ValueError
        elif self.quality == 1:
            self.draft - self.cargo*3.5
        elif self.quality == 0.5:
@@ -40,3 +40,10 @@ class Cruise(Ship):
    def __init__(self, passengers, draft, crew):
       Ship.__init__(self, draft, crew)
       self.passengers = passengers
+
+   def is_worth_it(self) ->float:
+      self.draft - self.passengers*2.25
+      self.draft - self.crew*1.5
+      
+      return self.draft 
+
