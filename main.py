dee436c818101949b64e51a94d2b41435c910aa2
Thiago Monteros <thmonteros2@gmail.com>
2023-08-12 23:13:04 -0300

    cambios en main

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-def main() -> None:
-  print("Hello World")
+import prueba
+import random
 
+def main() -> None:
+
+  random.seed()
+  dado = random.randint(1,3)
+
+  draft_aux = random.randint(1, 200)
+  crew_aux = random.randint(1, 100)
+  cargo_aux = random.randint(1, 100)
+  passengers_aux = random.randint(1,100)
+
+  if dado == 1:
+    Barco1 = prueba.Ship(draft_aux, crew_aux)
+    Barco1.is_worth_it()
+
+    
 if __name__ == "__main__":
   main()
