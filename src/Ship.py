3a2b081aea4a6c885be8de53375630addf106845
Thiago Monteros <thmonteros2@gmail.com>
2023-08-13 00:19:23 -0300

    Cambios y misterios

--- src/Ship.py
+++ src/Ship.py
@@ -1,12 +1,12 @@
 class Ship:
-  max_ = 200
-  def __init__(self, draft, crew):
-         self.draft = draft
-         self.crew = crew
+        max_ = 200
+def __init__(self, draft, crew):
+               self.draft = draft
+               self.crew = crew
 
-  def get_draft(self):
-         return self.draft
-  def get_crew(self):
-        return self.crew
-  def is_worth_it(self)->float:
-        return (self.draft-(self.crew*1.5))
+def is_worth_it():
+       if self.draft - (self.crew*1.5):
+          raise Exception("No vale la pena")
+        elif:
+          return self.draft
+
