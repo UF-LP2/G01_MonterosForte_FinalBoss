bdb1eedcab71a2ee0eee046e2c355a8289de9d67
Thiago Monteros <thmonteros2@gmail.com>
2023-08-14 21:10:20 -0300

    cambios y simplificacion de archivo

--- src/archivos.py
+++ src/archivos.py
@@ -3,7 +3,7 @@ import csv
 
 def readFile():
     barcos = []
-    with open(r"C:\Users\thmon\OneDrive\Documents\Universidad\Labo Progra 2\Final Boss Final\G01_MonterosForte_FinalBoss\ships.csv") as file:
+    with open(r".\ships.csv") as file:
         reader = csv.reader(file)
         for row in reader:
             barcos.append(row)
--- test/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-from pathlib import Path
-
-sys.path.append(Path(r"C:\Users\thmon\OneDrive\Documents\Universidad\Labo Progra 2\Final Boss Final\G01_MonterosForte_FinalBoss\test\test.py"))
