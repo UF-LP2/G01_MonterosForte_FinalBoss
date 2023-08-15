22d9b61ad6ffb24efe8829efefb1d80be3909d5c
Thiago Monteros <thmonteros2@gmail.com>
2023-08-15 18:50:37 -0300

    cambios en el test, yml agregado

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -2,11 +2,21 @@
 from src.Ship import Ship
 from src.Cargo import Cargo
 from src.Cruise import Cruise
+import pytest
 
 
 def test_Ship():
+    
     barco1 = Ship(200, 10)
     assert barco1.is_worth_it() == 185
+    barco2 = Ship(100, 30)
+    assert barco2.is_worth_it() == 55
+    
+
+def Exception_Test():
+    barco3 = Ship(50, 40) 
+    with pytest.raises(ValueError("No merece ser saqueado")):
+        barco3.is_worth_it()
 
 def test_Cruise():
     barco2 = Cruise(200 , 1000, 10)
