84a6f32790c2f97d5d29175bf26c8373904d33cc
Thiago Monteros <thmonteros2@gmail.com>
2023-08-15 19:57:30 -0300

    cambios en tests

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -2,6 +2,7 @@
 from src.Ship import Ship
 from src.Cargo import Cargo
 from src.Cruise import Cruise
+import pytest
 
 def test_Ship():
     
@@ -10,13 +11,27 @@ def test_Ship():
     barco2 = Ship(100, 30)
     assert barco2.is_worth_it() == 55
 
+    with pytest.raises(ValueError):
+        barco3 = Ship(100, 90) 
+        assert barco3.is_worth_it() == -35
+
+
 def test_Cruise():
-    Crucero1 = Cruise(200 , 1000, 10)
-    assert not Crucero1.is_worth_it() == 475 #535 es la correcta
-    Crucero2 = Cruise(100, 250, 2) 
-    assert Crucero2.is_worth_it() == 22
+    with pytest.raises(ValueError):
+        Crucero1 = Cruise(200 , 1000, 10)
+        assert not Crucero1.is_worth_it() == 475 #535 es la correcta
+        Crucero2 = Cruise(100, 250, 2) 
+        assert Crucero2.is_worth_it() == 22
+        Crucero3 = Cruise(100, 300, 100)
+        Crucero3.is_worth_it() < 20
 
 def test_Cargo():
-    Cargo1 = Cargo(50, 1, 300, 25)
-    assert not Cargo1.is_worth_it() == 86.5 #deberia ser 87.5
-    assert Cargo1.is_worth_it() == 87.5
+    with pytest.raises(ValueError):
+        Cargo1 = Cargo(50, 1, 300, 25)
+        assert not Cargo1.is_worth_it() == 86.5 #deberia ser 87.5
+        assert Cargo1.is_worth_it() == 87.5
+
+        Cargo2 = Cargo(100, 0.5, 100, 90)
+        assert Cargo2.is_worth_it() < 20
+
+
